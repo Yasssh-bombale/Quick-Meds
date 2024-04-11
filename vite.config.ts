5f9b60a1f190e9192c71acd7c50060f2e8fe05cb
Yash Vijay Bombale <yashbombale@gmail.com>
2024-04-11 16:43:00 +0530

    check

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://quick-meds.onrender.com",
+        target: "https://quick-meds.onrender.com/",
         secure: false,
       },
     },
