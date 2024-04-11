2a59f9bbf17bdfb86f176777cb97389a371c020f
Yash Vijay Bombale <yashbombale@gmail.com>
2024-04-11 09:33:33 +0530

    deployment check

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://quick-meds.vercel.app",
         secure: false,
       },
     },
